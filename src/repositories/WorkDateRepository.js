a0aba950271b14fc26f9663412df98e32e91b0d3
SeongUk52 <rkfka7849@gmail.com>
2026-01-06 15:23:19 +0900

    feat: 근무일 리포지토리 생성
    - 시작 요일과 월에 기반하여 근무 달력을 만든다.

---
+++ src/repositories/WorkDateRepository.js
@@ -0,0 +1,41 @@
+import WorkDate from "../domains/WorkDate.js";
+
+class WorkDateRepository {
+    static DAYS_IN_MONTH = [
+        31, // 1월
+        28, // 2월 (고정)
+        31, // 3월
+        30, // 4월
+        31, // 5월
+        30, // 6월
+        31, // 7월
+        31, // 8월
+        30, // 9월
+        31, // 10월
+        30, // 11월
+        31  // 12월
+    ];
+    static DAY_OF_THE_WEEK = [
+      '월', '화', '수', '목', '금', '토', '일'
+    ];
+
+    #workDates;
+
+    constructor(month, startDayOfTheWeek) {
+        this.#workDates = []
+
+        const daysInMonth = WorkDateRepository.DAYS_IN_MONTH[month - 1];
+        const startIndex = WorkDateRepository.DAY_OF_THE_WEEK.indexOf(startDayOfTheWeek);
+
+        for(let i = 0; i < daysInMonth; i++) {
+            const dayIndex = (startIndex + i) % 7;
+            const dayOfWeek = WorkDateRepository.DAY_OF_THE_WEEK[dayIndex];
+
+            this.#workDates.push(new WorkDate(i + 1, dayOfWeek))
+        }
+
+    }
+
+}
+
+export default WorkDateRepository;
